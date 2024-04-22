a15dc20ee5cb4e9297b674525b0ec9c496fba2c5
ZhannaKvl <kovalenko.zhanna1121@vu.cdu.edu.ua>
2024-04-22 21:33:16 +0200

    First commit
    OOP

---
+++ Assets/Scripts/BallTransform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTransform : MonoBehaviour
+{
+    public Vector3 scaleChange;
+    public float speed;
+    void Start()
+    {
+        speed = Random.value;
+    }
+
+    void Update()
+    {
+        transform.localScale += scaleChange;
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
